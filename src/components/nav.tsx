14098ecc18b3852325a089f20679f29dd1f3f94c
unknown <naphtalynonofo@gmail.com>
2025-03-03 15:36:07 +0200

    pulling services

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -11,7 +11,7 @@ function Nav() {
 
   return (
     <>
-      <header className="py-5 fixed w-full top-0 z-10" style={{ backgroundColor: "#6496B3" }}>
+      <header className="py-5 fixed w-full top-0 z-10 px-10" style={{ backgroundColor: "#6496B3" }}>
         <nav className="container mx-10 justify-between items-center flex">
           {/* Logo */}
           <Link to="/">
