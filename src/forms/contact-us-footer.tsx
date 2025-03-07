9f0cee9d21cc177321d252da4b1036ec2d5ae4cb
unknown <naphtalynonofo@gmail.com>
2025-03-07 12:42:21 +0200

    fixed the footer

--- src/forms/contact-us-footer.tsx
+++ src/forms/contact-us-footer.tsx
@@ -37,24 +37,26 @@ function ContactUsFooter() {
     try {
       if (!email) {
         alert("Please enter your email");
-        return; // Exit if email is not provided
+        return;
       }
-
+  
       const userData = {
         subject,
         email,
         message,
       };
-
+  
+      // console.log("Sending data:", userData);
+  
       const response = await axios.post(
         "https://zt-botswana.onrender.com/mail/send-email/",
         userData
       );
-      console.log(response.data);
-      alert("Message sent successfully!"); // Use alert instead of Alert.alert
+      console.log("Response:", response.data);
+      alert("Message sent successfully!");
     } catch (error) {
-      console.error(error);
-      alert("An error occurred while sending the message."); // Handle error gracefully
+      console.error("Error:", error); // Log the full error
+      alert("An error occurred while sending the message.");
     }
   };
 
@@ -72,7 +74,7 @@ function ContactUsFooter() {
             id="subject"
             name="subject"
             onChange={handleSubject}
-            className="peer w-full rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-900"
+            className="peer w-full rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-8 text-black placeholder-indigo-300 outline-none transition-colors duration-200 ease-in-out focus:bg-gray-100 focus:ring-2"
             placeholder="subject"
           />
 
@@ -83,23 +85,17 @@ function ContactUsFooter() {
 
           )}
 
-          {subject.length < 0 ? null : subjectVerify ? null : (
+          {subject.length < 1 ? null : subjectVerify ? null : (
             <p
               style={{
                 marginLeft: 20,
                 color: "red",
               }}
             >
-              Subject cannot be empty.
+              Subject too short.
             </p>
           )}
-
-          <label
-            htmlFor="subject"
-            className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:bg-gray-100 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-          >
-            Name
-          </label>
+          
         </div>
       </div>
       <div className="w-full p-1">
@@ -109,7 +105,7 @@ function ContactUsFooter() {
             id="email"
             name="email"
             onChange={handleEmail}
-            className="peer w-full rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-900"
+            className="peer w-full rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-8 text-black placeholder-indigo-300 outline-none transition-colors duration-200 ease-in-out focus:bg-gray-100 focus:ring-2"
             placeholder="Email"
           />
 
@@ -130,12 +126,6 @@ function ContactUsFooter() {
             </p>
           )}
 
-          <label
-            htmlFor="email"
-            className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:bg-gray-100 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-          >
-            Email
-          </label>
         </div>
       </div>
       <div className="mt-2 w-full p-1">
@@ -144,7 +134,7 @@ function ContactUsFooter() {
             id="message"
             name="message"
             onChange={handleMessage}
-            className="peer h-15 w-full resize-none rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-6 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-900"
+            className="peer h-13 w-full rounded border border-[#6496B3] bg-gray-100 bg-opacity-40 py-1 px-3 text-base leading-8 text-black placeholder-indigo-300 outline-none transition-colors duration-200 ease-in-out focus:bg-gray-100 focus:ring-2"
             placeholder="Message"
           ></textarea>
 
@@ -154,29 +144,23 @@ function ContactUsFooter() {
             <MdErrorOutline />
           )}
 
-          {message.length < 0 ? null : messageVerify ? null : (
+          {message.length < 1 ? null : messageVerify ? null : (
             <p
               style={{
                 marginLeft: 20,
                 color: "red",
               }}
             >
-              Message cannot be empty.
+              Message short.
             </p>
           )}
 
-          <label
-            htmlFor="message"
-            className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:bg-gray-100 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-          >
-            Message
-          </label>
         </div>
       </div>
       <div className="w-full p-1">
         <button
           onClick={handleSubmit}
-          className="mx-auto flex rounded border-0 bg-indigo-500 py-2 px-4 text-lg text-white hover:bg-indigo-600 focus:outline-none"
+          className="ml-auto relative rounded border-0 bg-indigo-500 py-2 px-3 text-white hover:bg-indigo-600 focus:outline-none"
         >
           Submit
         </button>
