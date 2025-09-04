1de68b5e6274fd092a35e6a91e8bb95ddde4159d
unknown <naphtalynonofo@gmail.com>
2025-09-04 12:00:48 +0200

    updating the primary sending email to outlook

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,10 +127,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_USE_TLS = True
+# EMAIL_PORT = 465
+EMAIL_PORT = 587
+# EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_TO_COMPANY = os.getenv('EMAIL_TO_COMPANY')
--- backend/mail/urls.py
+++ backend/mail/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GeneralMailView, DetailedMailView
+from .views import GeneralMailView, DetailedMailView, send_email_graph
 
 
 urlpatterns=[
     path('send-email/general/', GeneralMailView.as_view(), name="general-email"),
-    path('send-email/detailed/', DetailedMailView.as_view(), name="detailed-email")
+    path('send-email/detailed/', DetailedMailView.as_view(), name="detailed-email"),
+    path('send-email/sendmailgraph/', send_email_graph.as_view(), name="sendmail-graph")
     # path('send-email/', send_email, name="send-email")
 
 ]
